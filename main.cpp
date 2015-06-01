d1c4b9b9ecf29d185adb66ecba109e3222825f86
Danny Van Stemp <danwardvs@adsgames.net>
2015-05-31 21:21:21 -0400

    Cleaned up old crappy starter projects

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include<allegro.h>
 #include<alpng.h>
 #include<time.h>
-#include<iostream>
+#include <iostream>
 #include <fstream>
-#include<vector>
+#include <vector>
 #include<string>
 
 #include "rapidxml.hpp"
@@ -12,21 +12,24 @@
 using namespace rapidxml;
 using namespace std;
 
-BITMAP* al_buffer;
-
-bool close_button_pressed;
-
-int random_number=0;
+BITMAP* buffer;
 
 int step;
-int scrolly;
 
-char* pitcher1="NA";
-char* pitcher2="NA";
-char* pitcher3="NA";
-char* pitcher4="NA";
-char* pitcher5="NA";
-char* pitcher6="NA";
+int batter_number=1;
+
+// Fonts
+FONT* f1;
+FONT* f2;
+FONT* f3;
+FONT* f4;
+FONT* f5;
+FONT* ptsans;
+FONT* ptsans_big;
+FONT* ptsans_108;
+FONT* ptsans_48;
+
+bool close_button_pressed;
 
 // FPS System
 volatile int ticks = 0;
@@ -36,6 +39,31 @@ volatile int game_time = 0;
 int fps;
 int frames_done;
 int old_time;
+
+int color=1;
+
+int random_number;
+int random_number_steals_throws;
+int other_random_numbers[6];
+
+string pitcher1;
+string pitcher2;
+string pitcher3;
+string pitcher4;
+string pitcher5;
+string pitcher6;
+
+string pitcher_ss[6];
+string pitcher_gs[6];
+string pitcher_as[6];
+string pitcher_fs[6];
+
+string xml_file;
+
+int get_width;
+int get_height;
+
+int box_scale;
 
 void ticker(){
   ticks++;
@@ -51,22 +79,6 @@ void close_button_handler(void){
   close_button_pressed = TRUE;
 }
 END_OF_FUNCTION(close_button_handler)
-
-/*
-//Convert int to string
-string convertInt(int number){
-   stringstream ss;
-   ss << number;
-   return ss.str();
-}
-
-//Convert string to int
-int convertStringToInt(string newString){
-  int result;
-  stringstream(newString) >> result;
-  return result;
-}
-*/
 
 // Random number generator. Use int random(highest,lowest);
 int random(int newLowest, int newHighest)
@@ -88,40 +100,302 @@ void abort_on_error(const char *message){
 	 exit(-1);
 }
 
-void update(){
-  if(step>-1){
-    random_number++;
-    if(random_number==68)random_number=1;
-    step=0;
 
 
+void load_xml(){
+
+    xml_document<> doc;
+    xml_node<> * root_node;
+    // Read the xml file into a vector
+    if(batter_number==1)xml_file="1b.xml";
+    if(batter_number==2)xml_file="2b.xml";
+    if(batter_number==3)xml_file="3b.xml";
+    if(batter_number==4)xml_file="4b.xml";
+    if(batter_number==5)xml_file="5b.xml";
+    if(batter_number==6)xml_file="6b.xml";
+
+    ifstream theFile (xml_file.c_str());
+    vector<char> xml_buffer((istreambuf_iterator<char>(theFile)), istreambuf_iterator<char>());
+    xml_buffer.push_back('\0');
+    // Parse the buffer using the xml file parsing library into doc
+    doc.parse<0>(&xml_buffer[0]);
+    // Find our root node
+    root_node = doc.first_node("data");
+    // Iterate over the brewerys
+
+    for (xml_node<> * brewery_node = root_node->first_node("GeneratedNumber"); brewery_node; brewery_node = brewery_node->next_sibling())
+    {
+
+            // Interate over the beers
+      int generatedNumberResult = atoi(brewery_node->first_attribute("number")->value());
+      if(generatedNumberResult==random_number){
+
+        for(xml_node<> * beer_node = brewery_node->first_node("batResult"); beer_node; beer_node = beer_node->next_sibling())
+        {
+          pitcher1 = beer_node->first_attribute("pitcher1")->value();
+          pitcher2 = beer_node->first_attribute("pitcher2")->value();
+          pitcher3 = beer_node->first_attribute("pitcher3")->value();
+          pitcher4 = beer_node->first_attribute("pitcher4")->value();
+          pitcher5 = beer_node->first_attribute("pitcher5")->value();
+          pitcher6 = beer_node->first_attribute("pitcher6")->value();
+        }
+
+      }
+    }
+
+}
+void load_slugging_xml(string new_xml_file){
+
+    if(random_number>84 && random_number<101){
+
+    xml_document<> doc;
+    xml_node<> * root_node;
+    // Read the xml file into a vector
 
 
-  }
-  step++;
+    ifstream theFile (new_xml_file.c_str());
+    vector<char> xml_buffer((istreambuf_iterator<char>(theFile)), istreambuf_iterator<char>());
+    xml_buffer.push_back('\0');
+    // Parse the buffer using the xml file parsing library into doc
+    doc.parse<0>(&xml_buffer[0]);
+    // Find our root node
+    root_node = doc.first_node("data");
+    // Iterate over the brewerys
 
 
+    for (xml_node<> * brewery_node = root_node->first_node("GeneratedNumber"); brewery_node; brewery_node = brewery_node->next_sibling())
+    {
+
+            // Interate over the beers
+      int generatedNumberResult = atoi(brewery_node->first_attribute("number")->value());
+
+
+      if(generatedNumberResult==random_number){
+
+        for(xml_node<> * beer_node = brewery_node->first_node("batResult"); beer_node; beer_node = beer_node->next_sibling())
+        {
+          if(new_xml_file=="ss.xml"){
+          pitcher_ss[0] = beer_node->first_attribute("pitcher1")->value();
+          pitcher_ss[1] = beer_node->first_attribute("pitcher2")->value();
+          pitcher_ss[2] = beer_node->first_attribute("pitcher3")->value();
+          pitcher_ss[3] = beer_node->first_attribute("pitcher4")->value();
+          pitcher_ss[4] = beer_node->first_attribute("pitcher5")->value();
+          pitcher_ss[5] = beer_node->first_attribute("pitcher6")->value();
+          }
+          if(new_xml_file=="gs.xml"){
+          pitcher_gs[0] = beer_node->first_attribute("pitcher1")->value();
+          pitcher_gs[1] = beer_node->first_attribute("pitcher2")->value();
+          pitcher_gs[2] = beer_node->first_attribute("pitcher3")->value();
+          pitcher_gs[3] = beer_node->first_attribute("pitcher4")->value();
+          pitcher_gs[4] = beer_node->first_attribute("pitcher5")->value();
+          pitcher_gs[5] = beer_node->first_attribute("pitcher6")->value();
+          }
+          if(new_xml_file=="as.xml"){
+          pitcher_as[0] = beer_node->first_attribute("pitcher1")->value();
+          pitcher_as[1] = beer_node->first_attribute("pitcher2")->value();
+          pitcher_as[2] = beer_node->first_attribute("pitcher3")->value();
+          pitcher_as[3] = beer_node->first_attribute("pitcher4")->value();
+          pitcher_as[4] = beer_node->first_attribute("pitcher5")->value();
+          pitcher_as[5] = beer_node->first_attribute("pitcher6")->value();
+          }
+          if(new_xml_file=="fs.xml"){
+          pitcher_fs[0] = beer_node->first_attribute("pitcher1")->value();
+          pitcher_fs[1] = beer_node->first_attribute("pitcher2")->value();
+          pitcher_fs[2] = beer_node->first_attribute("pitcher3")->value();
+          pitcher_fs[3] = beer_node->first_attribute("pitcher4")->value();
+          pitcher_fs[4] = beer_node->first_attribute("pitcher5")->value();
+          pitcher_fs[5] = beer_node->first_attribute("pitcher6")->value();
+          }
+
+
+        }
+
+      }
+
+      }
+    }
 
 }
 
+void update(){
 
+    if(key[KEY_F1]){
+        set_gfx_mode(GFX_AUTODETECT_WINDOWED,1200,399, 0, 0);
+        box_scale = SCREEN_W/6;
+    }
+    if(key[KEY_F2]){
+        set_gfx_mode(GFX_AUTODETECT,get_width,get_height, 0, 0);
+        box_scale = SCREEN_W/6;
+    }
+
+
+    if((key[KEY_ENTER] || key[KEY_ENTER_PAD] || key[KEY_DEL] || key[KEY_SPACE] ||key[KEY_DEL_PAD] ) && step>9){
+
+        color++;
+        if(color==4)color=1;
+
+        for(int i; i<6; i++){
+          pitcher_ss[i]="";
+          pitcher_gs[i]="";
+          pitcher_as[i]="";
+          pitcher_fs[i]="";
+        }
+        for(int i; i<5; i++){
+          other_random_numbers[i]=random(1,100);
+        }
+
+        random_number=random(1,122);
+        random_number_steals_throws=random(1,122);
+        load_xml();
+        load_slugging_xml("ss.xml");
+        load_slugging_xml("gs.xml");
+        load_slugging_xml("as.xml");
+        load_slugging_xml("fs.xml");
+        step=0;
+    }
+    if(key[KEY_1] || key[KEY_1_PAD])batter_number=1;
+    if(key[KEY_2] || key[KEY_2_PAD])batter_number=2;
+    if(key[KEY_3] || key[KEY_3_PAD])batter_number=3;
+    if(key[KEY_4] || key[KEY_4_PAD])batter_number=4;
+    if(key[KEY_5] || key[KEY_5_PAD])batter_number=5;
+    if(key[KEY_6] || key[KEY_6_PAD])batter_number=6;
+
+    step++;
+}
 
 void draw(){
+    rectfill(buffer,0,0,SCREEN_W,SCREEN_H,makecol(255,255,255));
+
+    if(color==1)rectfill(buffer,0,348,SCREEN_W,398,makecol(255,255,0));
+    if(color==2)rectfill(buffer,0,348,SCREEN_W,398,makecol(255,0,0));
+    if(color==3)rectfill(buffer,0,348,SCREEN_W,398,makecol(0,0,255));
+
+    rect(buffer,0,348,SCREEN_W-1,398,makecol(0,0,0));
+
+    rectfill(buffer,205,10,390,135,makecol(0,0,0));
+    textprintf_ex(buffer,ptsans_108,210,-25,makecol(255,0,0),makecol(0,0,-1),"%i",random_number);
+    textprintf_ex(buffer,ptsans_big,25,13,makecol(0,0,0),makecol(0,0,-1),"%i",batter_number);
+    textprintf_ex(buffer,ptsans,10,10,makecol(0,0,0),makecol(0,0,-1),"Batter Number");
 
 
 
-    rectfill(al_buffer,0,0,SCREEN_W,SCREEN_H,makecol(255,255,255));
-    textprintf_ex(al_buffer,font,10,10,makecol(0,0,0),makecol(0,0,-1),"%i",random_number);
-    //load_xml();
+    rectfill(buffer,391,10,640,77,makecol(255,255,0));
+    rect(buffer,391,10,515,77,makecol(0,0,0));
+    rect(buffer,391,77,515,135,makecol(0,0,0));
 
-    textprintf_ex(al_buffer,font,10,50,makecol(0,0,0),makecol(0,0,-1),"1P:%s", pitcher1);
-     textprintf_ex(al_buffer,font,10,60,makecol(0,0,0),makecol(0,0,-1),"2P:%s", pitcher2);
-     textprintf_ex(al_buffer,font,10,70,makecol(0,0,0),makecol(0,0,-1),"3P:%s",pitcher3);
-     textprintf_ex(al_buffer,font,10,80,makecol(0,0,0),makecol(0,0,-1),"4P:%s",pitcher4);
-     textprintf_ex(al_buffer,font,10,90,makecol(0,0,0),makecol(0,0,-1),"5P:%s", pitcher5);
-     textprintf_ex(al_buffer,font,10,100,makecol(0,0,0),makecol(0,0,-1),"6P:%s",pitcher6);
+    rectfill(buffer,516,77,640,135,makecol(0,255,0));
+    rect(buffer,515,10,640,77,makecol(0,0,0));
+    rect(buffer,515,77,640,135,makecol(0,0,0));
 
-    draw_sprite(screen,al_buffer,0,0);
+    rectfill(buffer,641,10,765,135,makecol(51,51,153));
+    rect(buffer,640,10,765,135,makecol(0,0,0));
+    rect(buffer,640,77,765,135,makecol(0,0,0));
+
+
+    textprintf_ex(buffer,ptsans_48,420,4,makecol(0,0,0),makecol(0,0,-1),"%i",other_random_numbers[0]);
+    textprintf_ex(buffer,ptsans_48,544,4,makecol(0,0,0),makecol(0,0,-1),"%i",other_random_numbers[1]);
+    textprintf_ex(buffer,ptsans_48,668,65,makecol(255,255,255),makecol(0,0,-1),"%i",other_random_numbers[2]);
+    textprintf_ex(buffer,ptsans_48,668,4,makecol(255,255,255),makecol(0,0,-1),"%i",other_random_numbers[3]);
+
+    textprintf_ex(buffer,ptsans_48,544,65,makecol(0,0,0),makecol(0,0,-1),"%i",random_number_steals_throws);
+
+    textprintf_ex(buffer,ptsans,398,100,makecol(0,0,0),makecol(0,0,-1),"Steals and throws>");
+
+
+    rectfill(buffer,0,148,SCREEN_W-1,168,makecol(0,0,200));
+    rectfill(buffer,0,188,SCREEN_W-1,208,makecol(51,204,204));
+    rectfill(buffer,0,228,SCREEN_W-1,248,makecol(0,255,0));
+    rectfill(buffer,0,268,SCREEN_W-1,288,makecol(255,204,0));
+    rectfill(buffer,0,308,SCREEN_W-1,328,makecol(255,102,0));
+
+    rect(buffer,0,188,SCREEN_W-1,208,makecol(0,0,0));
+    rect(buffer,0,228,SCREEN_W-1,248,makecol(0,0,0));
+    rect(buffer,0,268,SCREEN_W-1,288,makecol(0,0,0));
+    rect(buffer,0,308,SCREEN_W-1,328,makecol(0,0,0));
+
+
+
+    rect(buffer,0,148,SCREEN_W-1,168,makecol(0,0,0));
+    rect(buffer,0,148,SCREEN_W-1,348,makecol(0,0,0));
+
+    //Vertical lines
+    rect(buffer,box_scale-10,148,(box_scale*2)-10,348,makecol(0,0,0));
+    rect(buffer,(box_scale*3)-10,148,(box_scale*4)-10,348,makecol(0,0,0));
+    rect(buffer,(box_scale*5)-10,148,SCREEN_W-1,348,makecol(0,0,0));
+
+
+    textprintf_ex(buffer,ptsans,10,170,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher1.c_str());
+    textprintf_ex(buffer,ptsans,box_scale,170,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher2.c_str());
+    textprintf_ex(buffer,ptsans,box_scale*2,170,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher3.c_str());
+    textprintf_ex(buffer,ptsans,box_scale*3,170,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher4.c_str());
+    textprintf_ex(buffer,ptsans,box_scale*4,170,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher5.c_str());
+    textprintf_ex(buffer,ptsans,box_scale*5,170,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher6.c_str());
+
+    textprintf_ex(buffer,ptsans,10,210,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_fs[0].c_str());
+    textprintf_ex(buffer,ptsans,box_scale,210,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_fs[1].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*2,210,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_fs[2].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*3,210,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_fs[3].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*4,210,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_fs[4].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*5,210,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_fs[5].c_str());
+
+    textprintf_ex(buffer,ptsans,10,250,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_as[0].c_str());
+    textprintf_ex(buffer,ptsans,box_scale,250,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_as[1].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*2,250,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_as[2].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*3,250,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_as[3].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*4,250,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_as[4].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*5,250,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_as[5].c_str());
+
+    textprintf_ex(buffer,ptsans,10,290,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_gs[0].c_str());
+    textprintf_ex(buffer,ptsans,box_scale,290,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_gs[1].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*2,290,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_gs[2].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*3,290,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_gs[3].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*4,290,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_gs[4].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*5,290,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_gs[5].c_str());
+
+    textprintf_ex(buffer,ptsans,10,330,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_ss[0].c_str());
+    textprintf_ex(buffer,ptsans,box_scale,330,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_ss[1].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*2,330,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_ss[2].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*3,330,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_ss[3].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*4,330,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_ss[4].c_str());
+    textprintf_ex(buffer,ptsans,box_scale*5,330,makecol(0,0,0),makecol(0,0,-1),"%s", pitcher_ss[5].c_str());
+
+
+    textprintf_ex(buffer,ptsans,10,150,makecol(255,255,255),makecol(0,0,-1),"Pitcher #1");
+    textprintf_ex(buffer,ptsans,box_scale,150,makecol(255,255,255),makecol(0,0,-1),"Pitcher #2");
+    textprintf_ex(buffer,ptsans,box_scale*2,150,makecol(255,255,255),makecol(0,0,-1),"Pitcher #3");
+    textprintf_ex(buffer,ptsans,box_scale*3,150,makecol(255,255,255),makecol(0,0,-1),"Pitcher #4");
+    textprintf_ex(buffer,ptsans,box_scale*4,150,makecol(255,255,255),makecol(0,0,-1),"Pitcher #5");
+    textprintf_ex(buffer,ptsans,box_scale*5,150,makecol(255,255,255),makecol(0,0,-1),"Pitcher #6");
+
+    textprintf_ex(buffer,ptsans,10,188,makecol(0,0,0),makecol(0,0,-1),"Fair Slugger");
+    textprintf_ex(buffer,ptsans,box_scale,188,makecol(0,0,0),makecol(0,0,-1),"Fair Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*2,188,makecol(0,0,0),makecol(0,0,-1),"Fair Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*3,188,makecol(0,0,0),makecol(0,0,-1),"Fair Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*4,188,makecol(0,0,0),makecol(0,0,-1),"Fair Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*5,188,makecol(0,0,0),makecol(0,0,-1),"Fair Slugger");
+
+    textprintf_ex(buffer,ptsans,10,228,makecol(0,0,0),makecol(0,0,-1),"Average Slugger");
+    textprintf_ex(buffer,ptsans,box_scale,228,makecol(0,0,0),makecol(0,0,-1),"Average Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*2,228,makecol(0,0,0),makecol(0,0,-1),"Average Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*3,228,makecol(0,0,0),makecol(0,0,-1),"Average Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*4,228,makecol(0,0,0),makecol(0,0,-1),"Average Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*5,228,makecol(0,0,0),makecol(0,0,-1),"Average Slugger");
+
+    textprintf_ex(buffer,ptsans,10,268,makecol(0,0,0),makecol(0,0,-1),"Good Slugger");
+    textprintf_ex(buffer,ptsans,box_scale,268,makecol(0,0,0),makecol(0,0,-1),"Good Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*2,268,makecol(0,0,0),makecol(0,0,-1),"Good Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*3,268,makecol(0,0,0),makecol(0,0,-1),"Good Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*4,268,makecol(0,0,0),makecol(0,0,-1),"Good Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*5,268,makecol(0,0,0),makecol(0,0,-1),"Good Slugger");
+
+    textprintf_ex(buffer,ptsans,10,308,makecol(0,0,0),makecol(0,0,-1),"Super Slugger");
+    textprintf_ex(buffer,ptsans,box_scale,308,makecol(0,0,0),makecol(0,0,-1),"Super Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*2,308,makecol(0,0,0),makecol(0,0,-1),"Super Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*3,308,makecol(0,0,0),makecol(0,0,-1),"Super Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*4,308,makecol(0,0,0),makecol(0,0,-1),"Super Slugger");
+    textprintf_ex(buffer,ptsans,box_scale*5,308,makecol(0,0,0),makecol(0,0,-1),"Super Slugger");
+
+    draw_sprite(screen,buffer,0,0);
 }
 
 
@@ -131,8 +405,41 @@ void draw(){
 
 
 void setup(){
-    al_buffer=create_bitmap(1024,768);
+    buffer=create_bitmap(get_width,get_height);
 
+
+    // Load fonts
+    f1 = load_font("ptsans.pcx", NULL, NULL);
+    f2 = extract_font_range(f1, ' ', 'A'-1);
+    f3 = extract_font_range(f1, 'A', 'Z');
+    f4 = extract_font_range(f1, 'Z'+1, 'z');
+    ptsans = merge_fonts(f4, f5 = merge_fonts(f2, f3));
+
+    f1 = load_font("ptsans_big.pcx", NULL, NULL);
+    f2 = extract_font_range(f1, ' ', 'A'-1);
+    f3 = extract_font_range(f1, 'A', 'Z');
+    f4 = extract_font_range(f1, 'Z'+1, 'z');
+    ptsans_big = merge_fonts(f4, f5 = merge_fonts(f2, f3));
+
+    f1 = load_font("ptsans_108.pcx", NULL, NULL);
+    f2 = extract_font_range(f1, ' ', 'A'-1);
+    f3 = extract_font_range(f1, 'A', 'Z');
+    f4 = extract_font_range(f1, 'Z'+1, 'z');
+    ptsans_108 = merge_fonts(f4, f5 = merge_fonts(f2, f3));
+
+    f1 = load_font("ptsans_48.pcx", NULL, NULL);
+    f2 = extract_font_range(f1, ' ', 'A'-1);
+    f3 = extract_font_range(f1, 'A', 'Z');
+    f4 = extract_font_range(f1, 'Z'+1, 'z');
+    ptsans_48 = merge_fonts(f4, f5 = merge_fonts(f2, f3));
+
+
+    // Destroy temporary fonts
+    destroy_font(f1);
+    destroy_font(f2);
+    destroy_font(f3);
+    destroy_font(f4);
+    destroy_font(f5);
 
     srand(time(NULL));
 
@@ -166,14 +473,14 @@ int main(){
   install_keyboard();
   install_mouse();
   set_color_depth(32);
+  get_desktop_resolution(&get_width,&get_height);
 
-
-  set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600, 0, 0);
+  set_gfx_mode(GFX_AUTODETECT,get_width,get_height, 0, 0);
   install_sound(DIGI_AUTODETECT,MIDI_AUTODETECT,".");
+  box_scale=SCREEN_W/6;
 
 
-
-  set_window_title("Dale's Game 2015");
+  set_window_title("SPBL Official Number Generator 1.0");
   setup();
 
 
